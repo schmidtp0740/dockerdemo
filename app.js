c7caccc0d4923f27dfcc7e88acea4007cf19bde9
schmidtp0740@ <schmidtp0740@gmail.com>
2018-04-16 10:00:50 -0500

    added code and docker files

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 8080
+app.get('/', (req, res) => res.send('Hello World everyone!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
